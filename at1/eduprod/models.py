ba731e3b123f7042be0dd2470e52a02188edd428
Mischa <mischaaaquisumbing@gmail.com>
2024-03-28 17:33:36 +1100

    Issues with login.html

--- at1/eduprod/models.py
+++ at1/eduprod/models.py
@@ -8,3 +8,5 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    
--- at1/eduprod/views.py
+++ at1/eduprod/views.py
@@ -8,3 +8,4 @@ def index(request):
     questions = Question.objects.all()
     questions_json = serializers.serialize('json', questions)
     return render(request, 'eduprod/index.html', {'questions_json': questions_json})
+
